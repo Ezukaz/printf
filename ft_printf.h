424bc7b9a31941c33fd381c0264783a45162a2e6
Kazuki Takahashi <katakaha@c5r2s5.42tokyo.jp>
2026-01-09 19:15:38 +0900

    First Submission 01091915

---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: katakaha <katakaha@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/17 12:12:49 by katakaha          #+#    #+#             */
+/*   Updated: 2026/01/08 16:55:18 by katakaha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINT_F_H
+# define PRINT_F_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+int	ft_printf(const char *, ...);
+
+#endif
