11b80013c42128cc549fc13a3195f5aab8d72e8c
Kazuki Takahashi <katakaha@c6r3s12.42tokyo.jp>
2026-01-13 11:36:35 +0900

    merge branches 01131133

--- ft_printf.h
+++ ft_printf.h
@@ -1,21 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf.h                                           :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: katakaha <katakaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 12:12:49 by katakaha          #+#    #+#             */
-/*   Updated: 2026/01/08 16:55:18 by katakaha         ###   ########.fr       */
+/*   Updated: 2026/01/13 11:28:45 by katakaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PRINT_F_H
 # define PRINT_F_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
 
 int	ft_printf(const char *, ...);
 
